c1e219089644971afe231a0537aab87f51b8ed5e
Nikolas LeBlanc <nikolas.leblanc@gmail.com>
2013-08-06 10:17:11 -0400

    Wordpress 3.6 and category structure

--- www/wordpress-default/wp-config-sample.php
+++ www/wordpress-default/wp-config-sample.php
@@ -16,16 +16,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'menopauseandu');
+define('DB_NAME', 'database_name_here');
 
 /** MySQL database username */
-define('DB_USER', 'menopauseandu');
+define('DB_USER', 'username_here');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'menopauseandu');
+define('DB_PASSWORD', 'password_here');
 
 /** MySQL hostname */
-define('DB_HOST', '192.168.0.91');
+define('DB_HOST', 'localhost');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
